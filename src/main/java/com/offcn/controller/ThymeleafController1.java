c4f531a011df3c4e49ab932821dfb1d8b489eeb4
lmh <344241432@qq.com>
2020-12-10 22:00:29 +0800

    循环遍历集合

--- src/main/java/com/offcn/controller/ThymeleafController1.java
+++ src/main/java/com/offcn/controller/ThymeleafController1.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class ThymeleafController1 {
 
@@ -20,5 +25,25 @@ public class ThymeleafController1 {
         Person person = new Person(2001, "小白", 12);
         model.addAttribute("person",person);
         return "person";
+    }
+
+    @GetMapping("/showList")
+    public String showList(Model model){
+        List list = new ArrayList();
+        for (int i=0;i<5;i++){
+            list.add(new Person(2000 + i ,"小花" + i,12 + i));
+        }
+        model.addAttribute("list",list);
+
+        List maps = new ArrayList();
+        for (int i=0;i<5;i++){
+            Map map = new HashMap();
+            map.put("name","map"+i);
+            map.put("id",2001 + i);
+            map.put("age",19 + i);
+            maps.add(map);
+        }
+        model.addAttribute("maps",maps);
+        return "list";
     }
 }
