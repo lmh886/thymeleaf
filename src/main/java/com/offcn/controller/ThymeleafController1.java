77e5064a56acb6bc29d34f72b81e4a34f7506ff0
lmh <344241432@qq.com>
2020-12-10 23:18:59 +0800

    基本应用全

--- src/main/java/com/offcn/controller/ThymeleafController1.java
+++ src/main/java/com/offcn/controller/ThymeleafController1.java
@@ -71,4 +71,21 @@ public class ThymeleafController1 {
         model.addAttribute("type","manager");
         return "switch";
     }
+
+    @GetMapping("/showStaticResource")
+    public String showStaticResource(){
+        return "showStatic";
+    }
+
+    @GetMapping("/showFragment")
+    public String showFragment(){
+        return "importFragment";
+    }
+
+    @GetMapping("/showInnerObject")
+    public String showInnerObject(Model model){
+        model.addAttribute("num",92345.12387);
+        model.addAttribute("str","携手最具影响力的中文知识平台，用知识的价值提升品牌的价值。针对不同类型的权威机构提供丰富的合作模式，开展基于知识传播的公益合作，强势展现合作方优质资源，提升品牌影响力。");
+        return "innerObject";
+    }
 }
